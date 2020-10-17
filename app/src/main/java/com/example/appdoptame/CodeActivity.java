c4cb943ecaab91693753ccf5b627ca8b21511ffb
Joan-hash <joanrojasp@yahoo.com>
2020-10-16 21:37:01 -0300

    creación de activitys principales, parte lógica y diseño

---
+++ app/src/main/java/com/example/appdoptame/CodeActivity.java
@@ -0,0 +1,14 @@
+package com.example.appdoptame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code);
+    }
+}
