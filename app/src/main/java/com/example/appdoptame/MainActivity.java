06259ec695bc338e4564424732a782fca6bb677b
Joan-hash <joanrojasp@yahoo.com>
2020-10-17 22:24:28 -0300

    creación de menu lateral, solamente diseño

--- app/src/main/java/com/example/appdoptame/MainActivity.java
+++ app/src/main/java/com/example/appdoptame/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.appdoptame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,49 @@ public class MainActivity extends AppCompatActivity {
             TextView mensaje = (TextView) findViewById(R.id.label_saludo);
             mensaje.setText(usuario + " ha conectado y pasado información de dos activitys");
         }
+
+        //agregar funcionalidades a los items del menu lateral
+        NavigationView nav = (NavigationView) findViewById(R.id.nav);
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.op_principal:
+                        //aqui va la intencion para cambiar de activity
+                        Toast.makeText(MainActivity.this, "Principal", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_mensajes:
+                        Toast.makeText(MainActivity.this, "Mis mensajes", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_consejos:
+                        Toast.makeText(MainActivity.this, "Consejos", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_agregar:
+                        Toast.makeText(MainActivity.this, "Agregar publicación", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_perfil:
+                        Toast.makeText(MainActivity.this, "Mi perfil", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_configuracion:
+                        Toast.makeText(MainActivity.this, "Configuración", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_terminos:
+                        Toast.makeText(MainActivity.this, "Términos y condiciones de uso", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_localizacion:
+                        Toast.makeText(MainActivity.this, "Localización", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.op_cerrar:
+                        Toast.makeText(MainActivity.this, "Cerrar sesión", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "La opción ingresada es incorrecta", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
