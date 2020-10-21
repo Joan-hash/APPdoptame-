c7cbb7a3c0fdf1193160d3e5989f4acba9f5afd0
Joan-hash <joanrojasp@yahoo.com>
2020-10-20 21:03:58 -0300

    creacion de list view, que servirá de base para vizualizar las publicaciones, solo parte ded diseño

--- app/src/main/java/com/example/appdoptame/MainActivity.java
+++ app/src/main/java/com/example/appdoptame/MainActivity.java
@@ -19,13 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //resivir los datos que se mandaron desde el login
-        Bundle datos = getIntent().getExtras();
-        if (datos != null){
-            String usuario = datos.getString("user");
-            TextView mensaje = (TextView) findViewById(R.id.label_saludo);
-            mensaje.setText(usuario + " ha conectado y pasado información de dos activitys");
-        }
+
         menuLateral();
     }
 
