24748045138d6d7d591762b290858fa6f84be832
Joan <joanrojasp@protonmail.com>
2020-12-20 15:36:47 -0300

    conexión con db externa, funcionalidad para iniciar sesión y registrar un usuario agregada

--- app/src/main/java/com/example/appdoptame/CreateActivity.java
+++ app/src/main/java/com/example/appdoptame/CreateActivity.java
@@ -5,16 +5,79 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateActivity extends AppCompatActivity {
-
+    EditText usuarioC, correoC, telefonoC, contraseñaC, confContraseñaC;
+    Button btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        //recuperacion de los campos del formulario
+        usuarioC = (EditText) findViewById(R.id.input_usuarioC);
+        correoC = (EditText) findViewById(R.id.input_correo);
+        telefonoC = (EditText) findViewById(R.id.input_telefono);
+        contraseñaC = (EditText) findViewById(R.id.input_contraseñaC);
+        confContraseñaC = (EditText) findViewById(R.id.input_conf_contarseñaC);
+        btnRegistrar = (Button) findViewById(R.id.button_crear);
+        //evento para el boton
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //comparacion de la contraseñas, primera validación
+                String pass1 = contraseñaC.getText().toString();
+                String pass2 = confContraseñaC.getText().toString();
+                if (pass1.equals(pass2)){
+                    //relizamos la insercion en la base de datos
+                    registrarUsuario("http://192.168.1.43/app/registrarUsuario.php");
+                }else{
+                    Toast.makeText(CreateActivity.this, "Las contraeñas no son iguales", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-    public void crearCuenta(View v){
-        Intent i = new Intent(getApplicationContext(), LoginActivity.class);
-        startActivity(i);
+    private void registrarUsuario(String URL){
+        StringRequest peticion = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("1")){
+                    finish();
+                }else{
+                    Toast.makeText(CreateActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+                Toast.makeText(CreateActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String, String>();
+                parametros.put("usuario", usuarioC.getText().toString());
+                parametros.put("correo", correoC.getText().toString());
+                parametros.put("telefono", telefonoC.getText().toString());
+                parametros.put("contraseña", contraseñaC.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue colaPeticiones = Volley.newRequestQueue(getApplicationContext());
+        colaPeticiones.add(peticion);
     }
 }
--- app/src/main/java/com/example/appdoptame/LoginActivity.java
+++ app/src/main/java/com/example/appdoptame/LoginActivity.java
@@ -5,48 +5,77 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText usuarioL, claveL;
+    Button btnIniciarSesion;
+    TextView btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-    //metodo para iniciar sesion
-    public void iniciarSesion(View v){
-        //recuperar el nombre de usuario
-        EditText vUsuario = (EditText) findViewById(R.id.input_usuario);
-        String usuario = vUsuario.getText().toString();
-        //recuperar la contraseña
-        EditText vClave = (EditText) findViewById(R.id.input_contraseña);
-        String contraseña = vClave.getText().toString();
-        //validar datos
-        if (usuario.equals("joan")) {
-            if (contraseña.equals("1234")) {
-                Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                i.putExtra("user", usuario);
+        //recuperacion de los campos del inicio de sesion, excepto el de recuperacion de la clave
+        usuarioL = (EditText) findViewById(R.id.input_usuarioL);
+        claveL = (EditText) findViewById(R.id.input_contraseñaL);
+        btnIniciarSesion = (Button) findViewById(R.id.button_iniciar);
+        btnRegistrar = (TextView) findViewById(R.id.label_crear);
+        //agregar el evento click para que no reenvie a otra activity
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), CreateActivity.class);
                 startActivity(i);
             }
-            else{
-                //contraseña incorrecta
-                Toast.makeText(this, "Contraseña incorrecta", Toast.LENGTH_SHORT).show();
+        });
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //peticion a la base de datos
+                validarUsuario("http://192.168.1.43/app/iniciarSesion.php");
             }
-        }
-        else {
-            //usuario incorrecto
-            Toast.makeText(this, "Usuario incorrecto", Toast.LENGTH_SHORT).show();
-        }
+        });
     }
-    //metodo para recuperar la contraseña
-    public void recuperarClave(View v){
-        Intent i = new Intent(getApplicationContext(), RecoveryActivity.class);
-        startActivity(i);
-    }
-    public void crearCuenta(View v){
-        Intent i = new Intent(getApplicationContext(), CreateActivity.class);
-        startActivity(i);
+    private void validarUsuario (String URL){
+        StringRequest peticion = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()){
+                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(i);
+                }else{
+                    Toast.makeText(LoginActivity.this, "Usuario o contraseña incorrecta", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("usuario", usuarioL.getText().toString());
+                parametros.put("contraseña", claveL.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue colaPeticion = Volley.newRequestQueue(getApplicationContext());
+        colaPeticion.add(peticion);
     }
 }
